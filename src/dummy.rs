62433b7cfaea4d40c8d7a00e2035312d16994e56
dignifiedquire <me@dignifiedquire.com>
2020-01-21 01:24:45 +0100

    add batch proving

--- src/dummy.rs
+++ src/dummy.rs
@@ -1,57 +1,38 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 extern crate bellperson;
+extern crate ff;
 extern crate paired;
 extern crate rand;
-extern crate ff;
 
 // For randomness (during paramgen and proof generation)
 use self::rand::{thread_rng, Rng};
 
 // Bring in some tools for using pairing-friendly curves
-use self::paired::{
-    Engine
-};
+use self::paired::Engine;
 
-use self::ff::{Field,PrimeField};
+use self::ff::{Field, PrimeField};
 
 // We're going to use the BLS12-381 pairing-friendly elliptic curve.
-use self::paired::bls12_381::{
-    Bls12,
-    Fr
-};
+use self::paired::bls12_381::{Bls12, Fr};
 
 // We'll use these interfaces to construct our circuit.
-use self::bellperson::{
-    Circuit,
-    ConstraintSystem,
-    SynthesisError
-};
+use self::bellperson::{Circuit, ConstraintSystem, SynthesisError};
 
 // We're going to use the Groth16 proving system.
 use self::bellperson::groth16::{
-    Proof,
-    generate_random_parameters,
-    prepare_verifying_key,
-    create_random_proof,
-    verify_proof,
+    create_random_proof, generate_random_parameters, prepare_verifying_key, verify_proof, Proof,
 };
 
+#[derive(Clone)]
 pub struct DummyDemo<E: Engine> {
     pub xx: Option<E::Fr>,
 }
 
-impl <E: Engine> Circuit<E> for DummyDemo<E> {
-    fn synthesize<CS: ConstraintSystem<E>>(
-        self,
-        cs: &mut CS
-    ) -> Result<(), SynthesisError>
-    {
-
+impl<E: Engine> Circuit<E> for DummyDemo<E> {
+    fn synthesize<CS: ConstraintSystem<E>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
         let mut x_val = E::Fr::from_str("2");
-        let mut x = cs.alloc(|| "", || {
-            x_val.ok_or(SynthesisError::AssignmentMissing)
-        })?;
+        let mut x = cs.alloc(|| "", || x_val.ok_or(SynthesisError::AssignmentMissing))?;
 
         for k in 0..1_000_000 {
             // Allocate: x * x = x2
@@ -59,17 +40,10 @@ impl <E: Engine> Circuit<E> for DummyDemo<E> {
                 e.square();
                 e
             });
-            let x2 = cs.alloc(|| "", || {
-                x2_val.ok_or(SynthesisError::AssignmentMissing)
-            })?;
+            let x2 = cs.alloc(|| "", || x2_val.ok_or(SynthesisError::AssignmentMissing))?;
 
             // Enforce: x * x = x2
-            cs.enforce(
-                || "",
-                |lc| lc + x,
-                |lc| lc + x,
-                |lc| lc + x2
-            );
+            cs.enforce(|| "", |lc| lc + x, |lc| lc + x, |lc| lc + x2);
 
             x = x2;
             x_val = x2_val;
@@ -79,7 +53,7 @@ impl <E: Engine> Circuit<E> for DummyDemo<E> {
             || "",
             |lc| lc + (x_val.unwrap(), CS::one()),
             |lc| lc + CS::one(),
-            |lc| lc + x
+            |lc| lc + x,
         );
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 extern crate bellperson;
+extern crate ff;
 extern crate paired;
 extern crate rand;
-extern crate ff;
+use bellperson::groth16::Parameters;
 use bellperson::{Circuit, ConstraintSystem, SynthesisError};
-use bellperson::groth16::{Parameters};
-use paired::{Engine};
 use ff::{Field, PrimeField};
+use paired::Engine;
 
 use std::fs::File;
 use std::io::prelude::*;
 
-use std::time::{Duration, Instant};
 use std::thread::sleep;
+use std::time::{Duration, Instant};
 
 mod dummy;
 
-fn main(){
+fn main() {
     env_logger::init();
+    use bellperson::groth16::{
+        create_random_proof, create_random_proof_batch, generate_random_parameters,
+        prepare_verifying_key, verify_proof, Proof,
+    };
     use paired::bls12_381::{Bls12, Fr};
     use rand::thread_rng;
-    use bellperson::groth16::{
-        create_random_proof, generate_random_parameters, prepare_verifying_key, verify_proof, Proof,
-    };
 
     println!("I know the value of 2^(2^1000)");
 
@@ -31,7 +32,7 @@ fn main(){
 
     println!("Creating parameters...");
 
-    let load_parameters = false;
+    let load_parameters = true;
     let parameters_path = "parameters.dat";
 
     // Create parameters for our circuit
@@ -40,36 +41,47 @@ fn main(){
         Parameters::<Bls12>::read(param_file, false /* false for better performance*/)
             .expect("Unable to read parameters file!")
     } else {
-        let c = dummy::DummyDemo::<Bls12> {
-            xx: None
-        };
+        let c = dummy::DummyDemo::<Bls12> { xx: None };
 
         let p = generate_random_parameters(c, rng).unwrap();
-        let mut param_file = File::create(parameters_path).expect("Unable to create parameters file!");
-        p.write(param_file).expect("Unable to write parameters file!");
+        let mut param_file =
+            File::create(parameters_path).expect("Unable to create parameters file!");
+        p.write(param_file)
+            .expect("Unable to write parameters file!");
         p
     };
 
     // Prepare the verification key (for proof verification)
     let pvk = prepare_verifying_key(&params.vk);
 
-    loop {
-        println!("Creating proofs...");
+    // Create an instance of circuit
+    let c = dummy::DummyDemo::<Bls12> {
+        xx: Fr::from_str("3"),
+    };
+
+    println!("Creating proofs...");
+    const SAMPLES: usize = 10;
+    for _ in 0..SAMPLES {
         let now = Instant::now();
 
-        // Create an instance of circuit
-        let c = dummy::DummyDemo::<Bls12> {
-            xx: Fr::from_str("3")
-        };
-
         // Create a groth16 proof with our parameters.
-        let proof = create_random_proof(c, &params, rng).unwrap();
-        println!("Total proof gen finished in {}s and {}ms", now.elapsed().as_secs(), now.elapsed().subsec_nanos()/1000000);
+        let proof = create_random_proof(c.clone(), &params, rng).unwrap();
+        println!(
+            "Single proof gen finished in {}s and {}ms",
+            now.elapsed().as_secs(),
+            now.elapsed().subsec_nanos() / 1000000
+        );
 
-        println!("{}", verify_proof(
-            &pvk,
-            &proof,
-            &[]
-        ).unwrap());
+        println!("{}", verify_proof(&pvk, &proof, &[]).unwrap());
     }
+    println!("Batch proof");
+    let now = Instant::now();
+
+    let proof = create_random_proof_batch(vec![c; SAMPLES], &params, rng).unwrap();
+    println!(
+        "Total proof batch gen finished in {}s and {}ms for {} proofs",
+        now.elapsed().as_secs(),
+        now.elapsed().subsec_nanos() / 1000000,
+        SAMPLES
+    );
 }
